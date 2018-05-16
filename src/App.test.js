f523624c976193285abbcf49d0e617f0667c17f4
a8568730 <a8568730@hotmail.com>
2018-05-16 17:15:12 +0800

    feat: 測試App按下按鈕後送出ajax。

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 import { shallow } from 'enzyme';
+import App from './App';
+import { 請求書寫檢查 } from './utils/請求';
 
 it('renders without crashing', () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-  // ReactDOM.unmountComponentAtNode(div);
   shallow(<App />);
 });
 
-// it('sums numbers', () => {
-//   expect(sum(1, 2)).toEqual(3);
-//   expect(sum(2, 2)).toEqual(4);
-// });
+it('按下去之後送ajax', () => {
+  const wrapper = shallow(<App />);
+  const 請求書寫檢查 = jest.fn();
+  wrapper.find('button').simulate('click');
+  expect(請求書寫檢查).toBeCalled();
+});
+
+// it('更新state書寫檢查')
+
+// it('state傳給顯示結果')
