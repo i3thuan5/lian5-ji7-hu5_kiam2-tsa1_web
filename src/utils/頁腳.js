9e5f4e1e6e2c307fded18f43fe14b510b76305f9
a8568730 <a8568730@hotmail.com>
2018-05-17 16:42:36 +0800

    feat: 加上頁腳。

---
+++ src/utils/頁腳.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Footer } from 'demo-ui';
+
+const sites = [
+  {
+    title: "Github",
+    url: "https://github.com/i3thuan5/lo5ma2ji7_tsuan2huan7",
+  },
+  {
+    title: "臺語羅馬字轉換器",
+    url: "https://xn--m7rr8i4sil03a3tchtsc3hztv.xn--v0qr21b.xn--kpry57d/",
+  },
+  {
+    title: "詞彙分級網站",
+    url: "https://xn--kbrr83a6ixbzl.xn--v0qr21b.xn--kpry57d/",
+  },
+  {
+    title: "台中教育大學台語系",
+    url: "http://taiwanese.ntcu.edu.tw/",
+  },];
+
+const Iah8Kha1 = () =>
+  <Footer sites={sites}/>;
+
+export default Iah8Kha1;
