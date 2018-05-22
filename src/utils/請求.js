55ccd55f8cc2e8a88b8b8a4bd2f3d8fefd8f7065
pigu <a8568730@gmail.com>
2018-05-22 17:59:01 +0800

    feat: 修改請求api串接對齊章物件。

--- src/utils/請求.js
+++ src/utils/請求.js
@@ -8,19 +8,34 @@
 }); */
 
 //static api
-export const 請求書寫檢查 = (漢字, 臺羅) => {
+export const 請求一句書寫檢查 = (漢字, 臺羅) => {
   return new Promise((resolve, reject) => {
-    resolve({結果: [
-      {漢字, 臺羅, 檢查: []},
-      {漢字, 臺羅, 檢查: ['1:3 違反動詞原則','1:3 違反動詞原則']},
-      {漢字, 臺羅, 檢查: []},
-    ]});
+    resolve({漢字, 臺羅, 檢查: ['1:3 違反動詞原則','1:3 違反動詞原則']});
   });
 };
 
-export const 請求對齊章物件 = (漢字, 臺羅) => {
-  return fetch('https://服務.意傳.台灣/漢字音標對齊', {
-    method: 'GET',
-    body: JSON.stringify({漢字, 臺羅, 查詢腔口: '閩南語'})
+export const 請求書寫檢查 = async function(對齊章物件) {
+
+  return new Promise(async (resolve, reject) => {
+    if (對齊章物件.hasOwnProperty('多元書寫')) {
+
+      await Promise.all(
+        對齊章物件.多元書寫.map(item => 請求一句書寫檢查(item.漢字, item.臺羅斷詞))
+      )
+      .then(values => resolve({結果: values}))
+      .catch(error => reject('請求書寫檢查發生錯誤'));
+
+    } else {
+      reject('對齊章物件應有多元書寫');
+    }
   });
+}
+
+export const 請求對齊章物件 = (漢字, 臺羅) => {
+  let url = new URL('https://服務.意傳.台灣/漢字音標對齊');
+  let params = {漢字, 音標: 臺羅, 查詢腔口: '閩南語'};
+  //manually append query paramters
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+  //request
+  return fetch(url, {method: 'GET'}).then(response => response.json());
 }
