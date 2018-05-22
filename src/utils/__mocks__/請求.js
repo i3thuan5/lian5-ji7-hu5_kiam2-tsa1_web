f9dea5822911e0313c3212eeb1c84e038fd8fa3a
pigu <a8568730@gmail.com>
2018-05-22 17:51:56 +0800

    feat: 修改mock的請求回傳格式。

--- src/utils/__mocks__/請求.js
+++ src/utils/__mocks__/請求.js
@@ -1,19 +1,21 @@
 
-
-export const 請求書寫檢查 = jest.fn(對齊結果 => {
-  let { 多元書寫 } = 對齊結果;
-  return new Promise((resolve, reject) => {
-    resolve({結果: [{...多元書寫[0], 檢查: []}]});
-  });
-});
-
-
-
 export const 請求對齊章物件 = jest.fn((漢字, 臺羅) => {
   return new Promise((resolve, reject) => {
     resolve({
       查詢腔口: '閩南語',
-      多元書寫: [{漢字, 臺羅}]
+      //假設只回傳一个對齊章物件的漢字臺羅
+      多元書寫: [{漢字, 臺羅斷詞: 臺羅}]
     });
+  });
+});
+
+export const 請求書寫檢查 = jest.fn(對齊結果 => {
+  let { 多元書寫 } = 對齊結果;
+  let { 漢字, 臺羅斷詞 } = 多元書寫[0];
+  return new Promise((resolve, reject) => {
+    resolve({結果: [
+      //根據對齊章物件，請求一組對齊句子的書寫檢查
+      {漢字, 臺羅: 臺羅斷詞, 檢查: []}
+    ]});
   });
 });
