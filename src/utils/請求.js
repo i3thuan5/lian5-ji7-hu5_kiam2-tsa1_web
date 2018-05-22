0a087c60eb1aab78982ca47594e43aea42f51819
pigu <a8568730@gmail.com>
2018-05-22 18:29:40 +0800

    feat: 串接全部的api，回傳書寫檢查結果。

--- src/utils/請求.js
+++ src/utils/請求.js
@@ -1,29 +1,39 @@
 
 
-/* export const 請求書寫檢查 = (漢字, 臺羅) =>
-  fetch('https://服務.意傳.台灣/書寫檢查', {
-     // regular fetch option
-    method: 'GET',
-    body: JSON.stringify({漢字, 臺羅})
-}); */
-
-//static api
 export const 請求一句書寫檢查 = (漢字, 臺羅) => {
-  return new Promise((resolve, reject) => {
-    resolve({漢字, 臺羅, 檢查: ['1:3 違反動詞原則','1:3 違反動詞原則']});
-  });
+  let url = new URL('https://詞彙分級資料庫.意傳.台灣/連字符');
+  let params = {漢字, 臺羅};
+  //manually append query paramters
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+  //request
+  console.log('依據', 漢字, 臺羅);
+//   return new Promise((resolve, reject) => {
+  return fetch(url, {method: 'GET'})
+    .then(response => {
+      return response.json();//this is a promise
+/*       console.log('response', dataPromise);
+          resolve({...dataPromise, 漢字, 臺羅}) */
+    })
+    .then(myjson => {
+      console.log('myjson', myjson, {...myjson, 漢字, 臺羅});
+      return Promise.resolve({...myjson, 漢字, 臺羅});
+    });
+//   });
 };
+
 
 export const 請求書寫檢查 = async function(對齊章物件) {
 
   return new Promise(async (resolve, reject) => {
     if (對齊章物件.hasOwnProperty('多元書寫')) {
-
-      await Promise.all(
+      let p = await Promise.all(
         對齊章物件.多元書寫.map(item => 請求一句書寫檢查(item.漢字, item.臺羅斷詞))
-      )
-      .then(values => resolve({結果: values}))
-      .catch(error => reject('請求書寫檢查發生錯誤'));
+      ).then(values => {
+        console.log('values=', values);
+        resolve({結果: values})
+      }).catch(error =>
+        reject('請求書寫檢查發生錯誤')
+      );
 
     } else {
       reject('對齊章物件應有多元書寫');
